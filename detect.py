5cf2d671e3820455da39ebdc2b8f17bed06ec068
Nostalgia-tim <147011974+Nostalgia-tim@users.noreply.github.com>
2024-04-24 22:35:53 -0400

    new mean

--- detect.py
+++ detect.py
@@ -42,31 +42,31 @@ def Estimated_speed(locations, fps, width):
     work_IDs = []
     work_IDs_index = []
     work_IDs_prev_index = []
-    work_locations = []  # 当前帧数据：中心点x坐标、中心点y坐标、目标序号、车辆类别、车辆像素宽度
-    work_prev_locations = []  # 上一帧数据，数据格式相同
+    work_locations = []  # Current frame data: center x coordinate, center y coordinate, target serial number, vehicle category, vehicle pixel width
+    work_prev_locations = []  # The format of the previous frame is the same
     speed = []
     for i in range(len(locations[1])):
-        present_IDs.append(locations[1][i][2])  # 获得当前帧中跟踪到车辆的ID
+        present_IDs.append(locations[1][i][2])  # Gets the ID of the vehicle tracked in the current frame
     for i in range(len(locations[0])):
-        prev_IDs.append(locations[0][i][2])  # 获得前一帧中跟踪到车辆的ID
+        prev_IDs.append(locations[0][i][2])  # Gets the ID traced to the vehicle in the previous frame
     for m, n in enumerate(present_IDs):
-        if n in prev_IDs:  # 进行筛选，找到在两帧图像中均被检测到的有效车辆ID，存入work_IDs中
+        if n in prev_IDs:  # Filter to find valid vehicle ids detected in both frames and store them in work_IDs
             work_IDs.append(n)
             work_IDs_index.append(m)
-    for x in work_IDs_index:  # 将当前帧有效检测车辆的信息存入work_locations中
+    for x in work_IDs_index:  # Stores the information about the vehicles effectively detected by the current frame in work_locations
         work_locations.append(locations[1][x])
     for y, z in enumerate(prev_IDs):
-        if z in work_IDs:  # 将前一帧有效检测车辆的ID索引存入work_IDs_prev_index中
+        if z in work_IDs:  # The ID index of the last valid detected vehicle is stored in the work_IDs_prev_index
             work_IDs_prev_index.append(y)
-    for x in work_IDs_prev_index:  # 将前一帧有效检测车辆的信息存入work_prev_locations中
+    for x in work_IDs_prev_index:  # Store the information about the vehicle that was effectively detected in the previous frame in work_prev_locations
         work_prev_locations.append(locations[0][x])
     for i in range(len(work_IDs)):
         speed.append(
-            math.sqrt((work_locations[i][0] - work_prev_locations[i][0]) ** 2 +  # 计算有效检测车辆的速度，采用线性的从像素距离到真实空间距离的映射
-                      (work_locations[i][1] - work_prev_locations[i][1]) ** 2) *  # 当视频拍摄视角并不垂直于车辆移动轨迹时，测算出来的速度将比实际速度低
+            math.sqrt((work_locations[i][0] - work_prev_locations[i][0]) ** 2 +  # Calculate the speed of the efficiently detected vehicle using a linear mapping from pixel distance to real space distance
+                      (work_locations[i][1] - work_prev_locations[i][1]) ** 2) *  # When the video shooting Angle is not perpendicular to the vehicle's moving trajectory, the measured speed will be lower than the actual speed
             width[work_locations[i][3]] / (work_locations[i][4]) * fps / 5 * 3.6 * 2)
     for i in range(len(speed)):
-        speed[i] = [round(speed[i], 1), work_locations[i][2]]  # 将保留一位小数的单位为km/h的车辆速度及其ID存入speed二维列表中
+        speed[i] = [round(speed[i], 1), work_locations[i][2]]  # The vehicle speed in km/h, with one decimal place reserved, and its ID are stored in the SPEED two-dimensional list
     return speed
 
 def detect(save_img=False):
@@ -79,11 +79,11 @@ def detect(save_img=False):
     save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
-    # 获得视频的帧宽高
+    # Get the frame width and height of the video
     capture = cv2.VideoCapture(source)
     frame_fature = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # deepsort模块初始化
+    # deepsort module initialization
     cfg = get_config()
     cfg.merge_from_file(opt.config_deepsort)
     deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
@@ -91,9 +91,9 @@ def detect(save_img=False):
                         nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                         max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                         use_cuda=True)
-    # 设置每种车型的真实车宽及人脑反应后的刹车时间
-    width = [0, 0.2, 1.85, 0.5, 0, 2.3, 0, 2.5]  # bicycle、car、motorcycle、bus、truck的实际宽度，单位m
-    time_person = 3   # 设置人脑反应后的刹车时间，单位为s，即从人反应后踩下刹车到车辆刹停的时间，这个时间与车辆本身的速度有关，后续可通过车机系统接口读取该速度，实现更好的预警效果
+    # Set the real width of each vehicle type and the braking time after human brain reaction
+    width = [0, 0.2, 1.85, 0.5, 0, 2.3, 0, 2.5]  # Actual width of bicycle, car, motorcycle, bus, truck, unit m
+    time_person = 3   # Set the braking time after the human brain reaction, the unit is s, that is, the time from the human reaction to the brake stop of the vehicle
     locations = []
     speed = []
     # Initialize
@@ -127,10 +127,10 @@ def detect(save_img=False):
     names = model.module.names if hasattr(model, 'module') else model.names
     # colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-    # 改变显示图片大小（自定义函数）
+    # Change the display image size (custom function)
     def cv_show(p, im0):
         height, width = im0.shape[:2]
-        a = 1200 / width  # 宽为1200，计算比例
+        a = 1200 / width  
         size = (1200, int(height * a))
 
         img_resize = cv2.resize(im0, size, interpolation=cv2.INTER_AREA)
@@ -181,31 +181,31 @@ def detect(save_img=False):
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # ---------------------------测速------------------------------------------#
+                # ---------------------------speed------------------------------------------#
                 bbox_xywh = []
                 confs = []
                 classes = []
                 img_h, img_w, _ = im0.shape
-                # 把im0的检测结果调整至deepsort的输入数据类型
+                # Adjust the im0 detection result to the input data type of deepsort
                 for *xyxy, conf, cls in det:
                     x_c, y_c, bbox_w, bbox_h = bbox_rel(img_w, img_h, *xyxy)
                     obj = [x_c, y_c, bbox_w, bbox_h]
                     bbox_xywh.append(obj)
                     confs.append([conf.item()])
                     classes.append([cls.item()])
-                xywhs = torch.Tensor(bbox_xywh)  # 调用Tensor类的构造函数__init__，生成单精度浮点类型的张量
+                xywhs = torch.Tensor(bbox_xywh)  # Call the __init__ constructor of the Tensor class to generate a tensor of single-precision floating-point type
                 confss = torch.Tensor(confs)
                 classes = torch.Tensor(classes)
-                # 把调整好的检测结果输入deepsort，并进行跟踪
+                # Input the adjusted results into deepsort and track them
                 outputs = deepsort.update(xywhs, confss, im0, classes)
                 box_centers = []
                 for i, each_box in enumerate(outputs):
-                    # 求得每个框的中心点
+                    # Find the center point of each box
                     if each_box[5] == 1 or each_box[5] == 2 or each_box[5] == 3 or each_box[5] == 5 or each_box[5] == 7:
                         box_centers.append([(each_box[0] + each_box[2]) / 2, (each_box[1] + each_box[3]) / 2, each_box[4],
                                         each_box[5], each_box[2] - each_box[0]])
                 location = box_centers
-                # 将每帧检测出来的目标中心坐标和车辆ID写入txt中,实现轨迹跟踪
+                # The target center coordinates and vehicle ID detected by each frame are written into txt to achieve trajectory tracking
                 if len(location) != 0:
                     with open('track.txt', 'a+') as track_record:
                         track_record.write('frame:%s\n' % str(frame_idx))
@@ -214,21 +214,21 @@ def detect(save_img=False):
                     print('done!')
                 locations.append(location)
                 print(len(locations))
-                # 每五帧写入一次测速的数据，进行测速
+                # The speed measurement data is written every five frames to measure the speed
                 if len(locations) == 5:
                     if len(locations[0]) and len(locations[-1]) != 0:
                         locations = [locations[0], locations[-1]]
                         speed = Estimated_speed(locations, fps, width)
                     with open('speed.txt', 'a+') as speed_record:
                         for sp in speed:
-                            speed_record.write('id:%s %skm/h\n' % (str(sp[1]), str(sp[0])))  # 将每辆车的速度写入项目根目录下的speed.txt中
+                            speed_record.write('id:%s %skm/h\n' % (str(sp[1]), str(sp[0])))  # Write the speed of each car to speed.txt in the root directory of the project
                     locations = []
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     conf2 = float(f'{conf:.2f}')
-                    if conf2 > 0.40:   # 置信度小于0.4时不显示检测框
-                        # --------------------若检测出来的目标属于bicycle、car、motorcycle、bus、truck之一，进行测距，排除其他无关目标的干扰-------------------#
+                    if conf2 > 0.40:   # When the confidence level is less than 0.4, the detection box is not displayed
+                        # if the detected target belongs to one of the bicycle, car, motorcycle, bus and truck, range and exclude the interference of other irrelevant goal
                         if names[int(cls)] == 'bicycle' or names[int(cls)] == 'car' or names[int(cls)] == 'motorcycle' or names[int(cls)] == 'bus' or names[int(cls)] == 'truck':
                             if save_txt:  # Write to file
                                 xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(
@@ -240,16 +240,16 @@ def detect(save_img=False):
                             if save_img or view_img:  # Add bbox to image
                                 label = f'{names[int(cls)]} {conf:.2f}'
                                 plot_one_box(xyxy, im0, speed, outputs, time_person, label=label, color=[0, 0, 255], line_thickness=3,
-                                             name=names[int(cls)])  # 调用函数进行不同类别的测距，并绘制目标框
+                                             name=names[int(cls)])  # Call the function to range different categories and draw the target box
 
 
 
             #  Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
 
-            # Stream results，检测后显示出来
+            # Stream results，
             if view_img:
-                cv_show(str(p), im0)   # 该自定义函数有resize函数重构图片大小，注意不能在检测之前直接resize图像大小，会影响测距结果
+                cv_show(str(p), im0)   # The custom function has the resize function to reconstruct the image size. Note that the image size cannot be directly resize before detection, which will affect the ranging result
 
             # Save results (image with detections)
             if save_img:
@@ -288,12 +288,12 @@ if __name__ == '__main__':
     parser.add_argument('--view-img', action='store_true', help='display results',default=True)
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')    # store_true为保存视频或者图片，路径为runs/detect
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')   
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')  # 结果视频的保存路径
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')  
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument("--config_deepsort", type=str, default="deep_sort/configs/deep_sort.yaml")
