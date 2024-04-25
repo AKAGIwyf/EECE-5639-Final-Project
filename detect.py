d6555aec7ea7a316ce75566f44cd8f05fad8b727
Nostalgia-tim <147011974+Nostalgia-tim@users.noreply.github.com>
2024-04-24 20:47:25 -0400

    parameters change

--- detect.py
+++ detect.py
@@ -280,7 +280,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov8s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='行车记录仪视频.mp4', help='source')  #  file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='video.mp4', help='source')  #  file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.01, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.01, help='IOU threshold for NMS')
