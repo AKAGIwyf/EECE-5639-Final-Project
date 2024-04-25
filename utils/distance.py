671614910cf8883ed83a2d14d3e4b2ad4f76d5fe
Nostalgia-tim <147011974+Nostalgia-tim@users.noreply.github.com>
2024-04-24 22:39:15 -0400

    Update distance.py

--- utils/distance.py
+++ utils/distance.py
@@ -1,18 +1,18 @@
-foc = 1.6        # 镜头焦距,单位为cm
-real_hight_bicycle = 26.04      # 自行车高度，注意单位是英寸
-real_hight_car = 59.08      # 轿车高度
-real_hight_motorcycle = 47.24      # 摩托车高度
-real_hight_bus = 125.98      # 公交车高度
-real_hight_truck = 137.79   # 卡车高度
+foc = 1.6        # Lens focal length, in cm
+real_hight_bicycle = 26.04      # The height of the bike, note that the units are inches
+real_hight_car = 59.08      # Car height
+real_hight_motorcycle = 47.24      # Motorcycle height
+real_hight_bus = 125.98      # Bus height
+real_hight_truck = 137.79   # Truck height
 ppi=400
 
-# 自定义函数，单目测距
+# Custom function, single visual distance measurement
 def detect_distance_car(h):
     dis_inch = (real_hight_car * foc) * ppi / (h - 4)
     dis_cm = dis_inch * 2.54
     dis_cm = int(dis_cm)
     dis_m = dis_cm/100
-    print(h)
+    #print(h)
     return dis_m
 
 def detect_distance_bicycle(h):
